48b554344b05d37440116866e7570f53608f0620
Tony-Elia <antony.safaga@gmail.com>
2025-05-14 19:40:14 +0300

    dishes DB connection

---
+++ dish_inventory/src/main/java/models/Dishes.java
@@ -0,0 +1,15 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Dishes {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	
+	String name;
+}
